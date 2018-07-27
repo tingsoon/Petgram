ab7797e6b2a53c1001e2b191dc0cd4d01973a52d
martinhilary <martinhilary96@gmail.com>
2018-07-27 10:52:19 +0800

    Left with routes

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,12 +1,12 @@
 class NotificationsController < ApplicationController
   def index
-  @notifications = current_user.notifications
+  	@notifications = current_user.notifications
   end
   
   def link_through
-  @notification = Notification.find(params[:id])
-  @notification.update read: true
-  redirect_to post_path @notification.post
+	  @notification = Notification.find(params[:id])
+	  @notification.update read: true
+	  redirect_to post_path @notification.post
   end
 
 end
